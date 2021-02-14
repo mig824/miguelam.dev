b0de23475e34952dbd445f70501b478a18879a56
mig824 <migmic93@gmail.com>
2021-02-13 22:59:45 -0800

    (adds) bio component for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import React, { FC } from 'react'
 
 import SEO from '../components/seo'
-import { Hero } from '../components/home'
+import { Hero, Bio } from '../components/home'
 
 const HomePage: FC = () => (
   <>
     <SEO title="Home" />
     <Hero />
-    <div>
-      <h2>Testing</h2>
-    </div>
+    <Bio />
   </>
 )
 
