82df06844732714528a7c79d447d03aed902564e
Miguel Michel <migmic93@gmail.com>
2021-04-24 21:30:19 -0700

    adds header component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import { Global as GlobalCSS } from '@emotion/react'
 import '@fontsource/yeseva-one'
 import '@fontsource/josefin-sans/300.css'
 
-// import Header from './header'
+import { Header } from './header'
 import { Footer } from './footer'
 import { globalCSS } from '@utils/design'
 
@@ -25,7 +25,7 @@ const Layout: FC = ({ children }) => {
   return (
     <>
       <GlobalCSS styles={globalCSS} />
-      {/* <Header author={site.siteMetadata?.author || `Author`} /> */}
+      <Header author={site.siteMetadata?.author || `Author`} />
       <main>{children}</main>
       <Footer metadata={site.siteMetadata} />
     </>
